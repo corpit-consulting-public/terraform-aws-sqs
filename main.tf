d94994aedb9c26a28c0cbfed2d40f76381d819d7
Nicolas Martinez <nmartinez@corpit-consulting.com>
2019-10-07 13:37:09 -0300

    support to fifo && sse

--- main.tf
+++ main.tf
@@ -1,3 +1,5 @@
+########################
+
 resource "aws_sqs_queue" "terraform_queue" {
   name                      = "${var.name}"
   delay_seconds             = "${var.delay_seconds}"
@@ -5,4 +7,32 @@ resource "aws_sqs_queue" "terraform_queue" {
   message_retention_seconds = "${var.message_retention_seconds}"
   receive_wait_time_seconds = "${var.receive_wait_time_seconds}"
   tags                      = "${var.tags}"
+}
+
+##########################With fifo_queue
+
+resource "aws_sqs_queue" "terraform_fifo_queue" {
+  count                       = "${ var.fifo_queue ? 1 : 0}"
+  name                        = "terraform-example-queue.fifo"
+  fifo_queue                  = "${var.fifo_queue}"
+  content_based_deduplication = "${var.content_based_deduplication}"
+  delay_seconds               = "${var.delay_seconds}"
+  max_message_size            = "${var.max_message_size}"
+  message_retention_seconds   = "${var.message_retention_seconds}"
+  receive_wait_time_seconds   = "${var.receive_wait_time_seconds}"
+  tags                        = "${var.tags}"
+}
+
+########################With Server-side encryption (SSE)
+
+resource "aws_sqs_queue" "terraform_sse_queue" {
+  count                             = "${var.has_sse ? 1 : 0}"
+  name                              = "terraform-example-queue"
+  kms_master_key_id                 = "${var.kms_master_key_id}"
+  kms_data_key_reuse_period_seconds = "${var.kms_data_key_reuse_period_seconds}"
+  delay_seconds                     = "${var.delay_seconds}"
+  max_message_size                  = "${var.max_message_size}"
+  message_retention_seconds         = "${var.message_retention_seconds}"
+  receive_wait_time_seconds         = "${var.receive_wait_time_seconds}"
+  tags                              = "${var.tags}"
 }
